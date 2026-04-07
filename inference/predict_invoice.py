9793b6aaa26172785c04851321535af409f0e9f0
jeet <2022bcaaidsjeet12881@poornima.edu.in>
2026-04-07 20:14:25 +0530

    Refactor model loading to remove dynamic path resolution and simplify load_model functionfrence

--- inference/predict_invoice.py
+++ inference/predict_invoice.py
@@ -1,25 +1,52 @@
-import os
 import joblib
 import pandas as pd
-import streamlit as st
 
-def get_model_path():
-    possible_paths = [
-        os.path.join(os.getcwd(), "models", "predict_flag_invoice.pkl"),
-        os.path.join(os.path.dirname(__file__), "..", "models", "predict_flag_invoice.pkl"),
-    ]
+MODEL_PATH = "models/predict_flag_invoice.pkl"
 
-    for path in possible_paths:
-        if os.path.exists(path):
-            print(f"Model found at: {path}")
-            return path
-
-    raise FileNotFoundError("Model file not found")
-
-def load_model():
-    model_path = get_model_path()
+def load_model(model_path: str = MODEL_PATH):
+    """
+    Load trained invoice flagging model.
+    """
     with open(model_path, "rb") as f:
         model = joblib.load(f)
     return model
 
-model = load_model()
+
+def predict_invoice_flag(input_data: dict):
+    """
+    Predict invoice flag using 5 features.
+    """
+    model = load_model()
+
+    # Convert to DataFrame
+    input_df = pd.DataFrame(input_data)
+
+    # REQUIRED FEATURES
+    required_cols = [
+        "invoice_quantity",
+        "invoice_dollars",
+        "Freight",
+        "total_item_quantity",
+        "total_item_dollars"
+    ]
+
+    # Correct variable name
+    input_df = input_df[required_cols]
+
+    # Prediction
+    input_df["Predicted_Flag"] = model.predict(input_df)
+
+    return input_df
+
+
+if __name__ == "__main__":
+    sample_data = {
+        "invoice_quantity": [10, 20, 90],
+        "invoice_dollars": [1000, 5000, 1000],
+        "Freight": [100, 300, 400],
+        "total_item_quantity": [10, 18, 80],
+        "total_item_dollars": [950, 4800, 8000]
+    }
+
+    prediction = predict_invoice_flag(sample_data)
+    print(prediction)
